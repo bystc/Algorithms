3df51c7be612d8ca2c18c17f3b569eed4e3fcb99
bystc <598296757@qq.com>
2017-03-03 20:11:06 +0800

    2017.3.3

---
+++ exercise1.1.9/src/exercise/Exercise.java
@@ -0,0 +1,18 @@
+package exercise;
+/*
+ * 将一个正整数N用二进制表示并转换为一个String类型的值s
+ * 
+ * 
+ * Integer.toBinaryString(N)
+ */
+public class Exercise {
+
+	public static void main(String[] args) {
+		String s="";
+		int N=5;
+		for(int n=N;n>0;n/=2)
+			s=(n%2)+s;
+		System.out.println(s);
+	}
+
+}
